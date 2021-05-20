1bb01bc16a7008c83353182dc3e61c06f52dafa8
shizhangchun <shichangchun@jd.com>
2021-05-20 11:28:43 +0800

    add "version": "1.0.8",

---
+++ lib/src/components/col/src/col.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="scc-col">
+    {{msg}}
+    <p class="scc-col-p1">111</p>
+    <p class="scc-col-p2">222</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'SccCol',
+  data () {
+    return {
+      msg: 'i am scc col'
+    }
+  }
+}
+</script>
---
+++ lib/src/components/row/src/row.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="scc-row">
+    {{msg}}
+     <p class="scc-row-p1">111</p>
+    <p class="scc-row-p2">222</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'SccRow',
+  data () {
+    return {
+      msg: 'i am scc row'
+    }
+  }
+}
+</script>
