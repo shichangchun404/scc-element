4ddbc9fdd7ae8ab0c0ac302d0c207880c4f7fd75
shizhangchun <shichangchun@jd.com>
2021-05-31 09:42:18 +0800

    add

--- src/components/canvas/src/canvas.vue
+++ src/components/canvas/src/canvas.vue
@@ -205,7 +205,7 @@ export default {
      * 从服务端拿截图，绘制到canvas
     */
     drawImageInCanvas(){
-      console.log("drawImageInCanvas 5")
+      console.log("drawImageInCanvas 6")
       var _this = this
 			let img = new Image();
       img.src = this.baseImageUrl
